f15d2a54f945a5e0fe5cfa10aa291492fa4fceee
unknown <adhamessam506@gmail.com>
2025-11-23 01:48:15 +0200

    fixs

--- lib/App/Router/main.dart
+++ lib/App/Router/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:tamkeen/App/Router/app_router.dart';
 import 'package:tamkeen/core/utils/app_theme.dart';
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  
   runApp(const Tamkeen());
-  
-  Future.delayed(const Duration(milliseconds: 500), () {
-    FlutterNativeSplash.remove();
-  });
 }
 
 class Tamkeen extends StatelessWidget {
