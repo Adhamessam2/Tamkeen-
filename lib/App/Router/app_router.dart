7fdff3e1956cac8aea4d165a471591b9d3de9523
unknown <adhamessam506@gmail.com>
2025-11-26 17:39:54 +0200

    fixs

--- lib/App/Router/app_router.dart
+++ lib/App/Router/app_router.dart
@@ -1,22 +1,19 @@
 import 'package:go_router/go_router.dart';
 import 'package:tamkeen/features/Auth/presentation/pages/login.dart';
 import 'package:tamkeen/features/Auth/presentation/pages/signup.dart';
+import 'package:tamkeen/features/Home/home.dart';
+import 'package:tamkeen/features/onboardings/presentation/pages/onboardings.dart';
 import 'package:tamkeen/features/splash_screen/splash_lottie';
 
 final GoRouter router = GoRouter(
-  initialLocation: '/splash',
   routes: [
+    GoRoute(path: '/', builder: (context, state) => const CustomSplashScreen()),
     GoRoute(
-      path: '/splash',
-      builder: (context, state) => const CustomSplashScreen(),
+      path: '/onboarding',
+      builder: (context, state) => const Onboarding(),
     ),
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const Login(),
-    ),
-    GoRoute(
-      path: '/signup',
-      builder: (context, state) => const Signup(),
-    ),
+    GoRoute(path: '/login', builder: (context, state) => const Login()),
+    GoRoute(path: '/signup', builder: (context, state) => const Signup()),
+    GoRoute(path: '/home', builder: (context, state) => const Home()),
   ],
 );
